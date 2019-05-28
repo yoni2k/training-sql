b608e7ffa2fc1777c7b2ef56556fbedbeaac1ba0
yoni2k <yoni.k81@gmail.com>
2019-05-28 14:47:36 +0300

    Playing with different options of WHERE

--- scripts/Test_Populate.sql
+++ scripts/Test_Populate.sql
@@ -1,7 +1,10 @@
 USE contacts;
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First1', 'Last1');
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First2', 'Last2');
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First3', 'Last3');
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First1', 'Last1');
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First1', 'Last4');
-INSERT INTO contacts.person (firstName, lastName) VALUES ('First4', 'Last1');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First1', 'Last1', '1980-01-01');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First2', 'Last2', '1980-01-01');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First3', 'Last3', '1980-01-01');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First1', 'Last1', '1981-01-01');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First1', 'Last4', '1982-01-01');
+INSERT INTO contacts.person (firstName, lastName, dateOfBirth) VALUES ('First4', 'Last1', '1980-01-01');
+
+-- No dateOfBirth
+INSERT INTO contacts.person (firstName, lastName) VALUES ('FirstNoDOB', 'LastNoDOB');
